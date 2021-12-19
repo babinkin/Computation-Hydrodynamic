8ec5f4ec67a2e135ec15406335225ff4c656ab63
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-12-20 00:09:19 +0300

    Solve Time Step Explicit Eiler

---
+++ 6) RieamannProblem/SolveTimeStepExplicitEiler.m
@@ -0,0 +1,7 @@
+function dt = SolveTimeStepExplicitEiler(U1,U2,U3,dx,Courant,gamma)
+rho = U1;
+u = U2./rho;
+p = (gamma-1)*(U3 - 0.5*(u.^2).*rho);
+c = sqrt(gamma*p./rho);
+dt = min( Courant*dx./(abs(u)+c) );
+end
